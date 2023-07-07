47d6e27ca79c9b90cc2d43fcf80aa30260d36962
conglt <thanhcong1710@gmail.com>
2023-07-07 17:26:21 +0700

    udpate

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,31 @@
-const webpack = require('webpack');
+/*=========================================================================================
+  File Name: vue.config.js
+  Description: configuration file of vue
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
 
 module.exports = {
+  publicPath: '/',
+  transpileDependencies: [
+    'vue-echarts',
+    'resize-detector'
+  ],
   configureWebpack: {
-    // Set up all the aliases we use in our app.
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 6
-      })
-    ]
-  },
-  pwa: {
-    name: 'Vue Argon Design',
-    themeColor: '#172b4d',
-    msTileColor: '#172b4d',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#172b4d'
-  },
-  css: {
-    // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
   }
-};
+  // devServer: {
+  //   overlay: {
+  //     warnings: true,
+  //     errors: true
+  //   }
+  // }
+}
+
